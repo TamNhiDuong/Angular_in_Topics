7d75ea270d71725a9d1221cfdb83a70bf7f1f6dc
TamNhiDuong <yanxinyee@gmail.com>
2020-02-16 19:44:59 +0200

    use service inside service with @Injectable

--- Angular-service/src/app/account/account.component.ts
+++ Angular-service/src/app/account/account.component.ts
@@ -1,25 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
-import { LoggingService } from '../services/logging.service';
 import {AccountService} from '../services/account.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
   constructor(
-    private loggingService: LoggingService,
     private accountService: AccountService) {
   }
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status)
-    this.loggingService.logStatusChange(status)
   }
 }
--- Angular-service/src/app/new-account/new-account.component.ts
+++ Angular-service/src/app/new-account/new-account.component.ts
@@ -7,17 +7,14 @@ import {AccountService} from '../services/account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
   constructor(
-    private loggingService: LoggingService, 
     private accountService: AccountService) {
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus)
-    this.loggingService.logStatusChange(accountStatus)
   }
 } 
