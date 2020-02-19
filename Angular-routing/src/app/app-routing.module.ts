e722aff40506a17deac7193fd5dab19a7853ad53
TamNhiDuong <yanxinyee@gmail.com>
2020-02-19 11:46:29 +0200

    split app-routing.module

---
+++ Angular-routing/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {Route, RouterModule} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes = [
+    { path: '', component: HomeComponent},
+    { path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent},
+    ]},
+    { path: 'servers', component: ServersComponent, children: [
+      { path: ':id/:edit', component: EditServerComponent},
+      { path: ':id', component: ServerComponent}
+    ]},
+    { path: 'not-found', component: NotFoundComponent},
+    { path: '**', redirectTo: '/not-found'}
+  ]
+@NgModule({
+imports: [
+    RouterModule.forRoot(appRoutes)
+],
+exports: [RouterModule]
+})
+export class AppRoutingModule {}
