f34ea3cfd3e87229ddb2420ae8d3043cfe05e791
TamNhiDuong <yanxinyee@gmail.com>
2020-03-02 22:39:59 +0200

    error, complete in observables

--- Observables/src/app/home/home.component.ts
+++ Observables/src/app/home/home.component.ts
@@ -19,12 +19,27 @@ export class HomeComponent implements OnInit {
         let count = 0;
         setInterval(() => {
           observer.next(count);
+          if(count >= 2) {
+            observer.complete();
+          };
+          if (count === 3) {
+            observer.error(new Error('Error!'))
+          }
           count++;
         }, 1000)
       });
 
-      this.firstObSubscription = customObservable.subscribe(data =>
-        console.log(data));
+      this.firstObSubscription = customObservable.subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          alert(error.message);
+        },
+        ()=> {
+          console.log('completed');
+        });
     }
 
   ngOnDestroy(): void {
